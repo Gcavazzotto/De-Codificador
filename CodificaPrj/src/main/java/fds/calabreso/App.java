6af8a80c63e137df1fb2c937a6504a8c8b813095
Guilherme Sanches Cavazzotto <163131471+Gcavazzotto@users.noreply.github.com>
2025-08-11 21:36:51 +0000

    Colocando os arquivos

---
+++ CodificaPrj/src/main/java/fds/calabreso/App.java
@@ -0,0 +1,7 @@
+package fds.calabreso;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, world! Projeto CodificaPrj criado com sucesso.");
+    }
+}
